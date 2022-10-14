75c59db5c42da46f89ac120890298f133ada8afa
A1 <portnovivan2011@mail.ru>
2022-10-14 21:19:37 +0300

    service 2
    (cherry picked from commit b08f2490f28458ef5b58e2d81a43c3c4ed79dc23)

--- src/main/java/com/epam/rd/autocode/EmployeeServiceImp.java
+++ src/main/java/com/epam/rd/autocode/EmployeeServiceImp.java
@@ -19,70 +19,108 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class EmployeeServiceImp implements EmployeeService {
     private EmployeeDao employeeDao = new EmployeeDaoImp();
+    private static final Comparator<Employee> SORT_BY_HIRE_DATE = Comparator.comparing(Employee :: getHired);
+    private static final Comparator<Employee> SORT_BY_LASTNAME= Comparator.comparing(e -> e.getFullName().getLastName());
+    private static final Comparator<Employee> SORT_BY_SALARY= Comparator.comparing(Employee :: getSalary);
+    private static final Comparator<Employee> REVERSE_SORT_BY_SALARY= Comparator.comparing(Employee :: getSalary).reversed();
+    private static final Comparator<Employee> SORT_BY_DEPARTMENT_NAME = (o1, o2) -> {
+        if(o1.getDepartment() != null && o2.getDepartment() != null){
+            return o1.getDepartment().getName().compareTo(o2.getDepartment().getName());
+        } else if(o1.getDepartment() == null){
+            return -1;
+        } else {
+            return 1;
+        }
+    };
+//    private static Predicate<Employee> GET_BY_DEPARTMENT_ID = e -> e.getDepartment().getId().equals(department.getId());
 
     @Override
     public List<Employee> getAllSortByHireDate(Paging paging) {
         List<Employee> employees = employeeDao.getAll();
-        employees.sort(Comparator.comparing(Employee :: getHired));
+        employees.sort(SORT_BY_HIRE_DATE);
         return getSublist(employees, paging);
     }
 
     @Override
     public List<Employee> getAllSortByLastname(Paging paging) {
-        return null;
+        List<Employee> employees = employeeDao.getAll();
+        employees.sort(SORT_BY_LASTNAME);
+        return getSublist(employees, paging);
     }
 
     @Override
     public List<Employee> getAllSortBySalary(Paging paging) {
-        return null;
+        List<Employee> employees = employeeDao.getAll();
+        employees.sort(SORT_BY_SALARY);
+        return getSublist(employees, paging);
     }
 
     @Override
     public List<Employee> getAllSortByDepartmentNameAndLastname(Paging paging) {
-        return null;
+        List<Employee> employees = employeeDao.getAll();
+        employees.sort(SORT_BY_LASTNAME);
+        employees.sort(SORT_BY_DEPARTMENT_NAME);
+        return getSublist(employees, paging);
     }
 
     @Override
     public List<Employee> getByDepartmentSortByHireDate(Department department, Paging paging) {
-        return null;
+        List<Employee> employees = employeeDao.getByDepartment(department);
+        employees.sort(SORT_BY_HIRE_DATE);
+        return getSublist(employees, paging);
     }
 
     @Override
     public List<Employee> getByDepartmentSortBySalary(Department department, Paging paging) {
-        return null;
+        List<Employee> employees = employeeDao.getByDepartment(department);
+        employees.sort(SORT_BY_SALARY);
+        return getSublist(employees, paging);
     }
 
     @Override
     public List<Employee> getByDepartmentSortByLastname(Department department, Paging paging) {
-        return null;
+        List<Employee> employees = employeeDao.getByDepartment(department);
+        employees.sort(SORT_BY_LASTNAME);
+        return getSublist(employees, paging);
     }
 
     @Override
     public List<Employee> getByManagerSortByLastname(Employee manager, Paging paging) {
-        return null;
+        List<Employee> employees = employeeDao.getByManager(manager);
+        employees.sort(SORT_BY_LASTNAME);
+        return getSublist(employees, paging);
     }
 
     @Override
     public List<Employee> getByManagerSortByHireDate(Employee manager, Paging paging) {
-        return null;
+        List<Employee> employees = employeeDao.getByManager(manager);
+        employees.sort(SORT_BY_HIRE_DATE);
+        return getSublist(employees, paging);
     }
 
     @Override
     public List<Employee> getByManagerSortBySalary(Employee manager, Paging paging) {
-        return null;
+        List<Employee> employees = employeeDao.getByManager(manager);
+        employees.sort(SORT_BY_SALARY);
+        return getSublist(employees, paging);
     }
 
     @Override
     public Employee getWithDepartmentAndFullManagerChain(Employee employee) {
-        return null;
+        return employeeDao.getByIdWithFullChain(employee.getId());
     }
 
     @Override
     public Employee getTopNthBySalaryByDepartment(int salaryRank, Department department) {
-        return null;
+        List<Employee> employees = employeeDao.getByDepartment(department);
+        employees.sort(REVERSE_SORT_BY_SALARY);
+        int lastIndex = salaryRank-1;
+        return employees.get(lastIndex);
     }
 
     private <T> List<T> getSublist(List<T> list, Paging paging) {
